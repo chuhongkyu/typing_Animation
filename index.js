821fde3463f7e1d0990e7a7c2861d53bcb204b7f
Mr.chu <chuhongkyu@gmail.com>
2022-03-30 14:50:49 +0900

    update

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-let  target = document.querySelector("#dynamic");
-
+let target = document.querySelector("#dynamic");
 
 function randomString(){
     let stringArr = ["Lear to HTML","Lear to CSS","Lear to JAVA","Lear to Recycle"];
     let selectString = stringArr[Math.floor(Math.random()* stringArr.length)];
     let selectStringArr = selectString.split("");
-
     return selectStringArr;
-
 }
 
 function resetTyping(){
     target.textContent = "";
     dyamic(randomString());
 }
-//한글자씩 텍스트 출력 함수
 
 function dyamic(randomArr){
-    console.log(randomArr);
+    // console.log(randomArr);
     if(randomArr.length > 0){
         target.textContent += randomArr.shift();
         setTimeout(function(){dyamic(randomArr);},80)
     }
-    else{
+     else{
         setTimeout(resetTyping, 3000);
     }
 }
@@ -35,3 +31,36 @@ function blink(){
 
 setInterval(blink, 500);
 
+const rainbowCycle = document.querySelector(".material-icons")
+
+function changeRGB(){
+    let r = Math.round(Math.random() * 255); 
+    let g = Math.round(Math.random() * 255);
+    let b = Math.round(Math.random() * 255);
+    // 0~255값을 랜덤으로 뽑아내서 각각의 변수 r,g,b 에 들어간다.
+ 
+    let random_color = "rgb(" + r + "," + g + "," + b + ")";
+    // random_color = rgb(r,g,b)
+ 
+    rainbowCycle.style.color = random_color; 
+    console.log(rainbowCycle.style.color);
+}   
+
+setInterval(changeRGB, 800);
+
+let NumberBox = document.querySelector("#mini_text");
+let count = 0;
+
+function NumberUP(){
+    let counting = setInterval(function(){
+        if(count == 999){
+            console.log(count);
+            document.body.style.backgroundImage = 'linear-gradient(to bottom, whihe, white)';
+            clearInterval(counting);
+            return false;
+        }
+        count += 1;
+        NumberBox.innerHTML = new Intl.NumberFormat().format(count);
+    }, 10);
+}
+NumberUP();
