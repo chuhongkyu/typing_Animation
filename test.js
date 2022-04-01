f3ddda675d0024bb5538011fed1b405ee02e6fe6
Mr.chu <chuhongkyu@gmail.com>
2022-04-01 13:21:30 +0900

    update

---
+++ test.js
@@ -0,0 +1,17 @@
+// const catName = "나비야"; 
+
+// console.log(catName, '이리오렴');
+// console.log(catName + " 이리오렴");
+// console.log(`${catName} 이리오렴`);
+
+// for(let i=0; i<3; i++ ){
+//     console.log(catName, '이리오렴 반복이다');
+// }
+
+const catNames = ["나비야", "고양시", "길냥이"]; 
+
+console.log(catNames.length);
+
+for(let i= 0; i< catNames.length; i++){
+   console.log(catNames[i]);
+}
