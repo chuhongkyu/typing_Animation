abeb4c3c7a35d9b7ec7d8e730fe873790d09da8d
Mr.chu <chuhongkyu@gmail.com>
2022-04-04 10:22:40 +0900

    update

--- test.js
+++ test.js
@@ -1,18 +1,21 @@
 const catNames = ["나비야", "고양시", "길냥이"]; 
 
-let counter =0;
+let counter = 0;
 
 function callName(){
-    console.log(catNames[1]);
+    for(let i =0; i< catNames.length; i++){
+        console.log(catNames[i])
+    }
     counter++;
-    console.log(counter);
+    console.log(counter, "번");
 
     if(counter == 3){
     clearInterval(callInterval);
+    console.log("종료")
  }
 }
    
-let callInterval = setInterval(callName ,3000);
+const callInterval = setInterval(callName ,3000);
 
 
 
