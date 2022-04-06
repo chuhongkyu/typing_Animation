fb9e2af581b54a0b68e383b310d7fd66a60c4a0b
Mr.chu <chuhongkyu@gmail.com>
2022-04-06 14:35:48 +0900

    update

---
+++ test2.js
@@ -0,0 +1,18 @@
+const pets = ["강아지", "고양이", "앵무새", "랫서팬더", "패릿"];
+
+console.log(pets);
+
+// for(let i = 0; i < pets.length; i++){
+//     console.log(i + pets[i]);
+// }
+
+pets.forEach(printpet);
+
+function printpet(item, index, array){
+    console.log(item, index, array);
+};
+
+// pets.forEach((item, index, array)=>{
+//     console.log(item, index, array);
+// });
+
