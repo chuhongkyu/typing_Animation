1c982b34b5df83fd89ba9196b189058cf2487dda
Mr.chu <chuhongkyu@gmail.com>
2022-04-07 16:12:06 +0900

    update

--- starbucks.js
+++ starbucks.js
@@ -1,15 +1,15 @@
-const LEFTBOX = document.querySelector("#LeftBox");
-const RIGHTBOX = document.querySelector(".photo_card");
+let LEFT_BOX = document.querySelector("#LeftBox");
+let RIGHT_BOX = document.querySelector(".photo_card");
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
 
     if(value > 600){
-        LEFTBOX.style.animation = "LeftBoxDisappear 1s ease-out forwards";
-        RIGHTBOX.style.animation = "RightBoxDisappear 1s ease-out forwards";
+        LEFT_BOX.style.animation = "LeftBoxDisappear 1s ease-out forwards";
+        RIGHT_BOX.style.animation = "RightBoxDisappear 1s ease-out forwards";
     }
     else{
-        LEFTBOX.style.animation = "LeftBoxMove 1s ease-out";
-        RIGHTBOX.style.animation = "RightBoxMove 1s ease-out";
+        LEFT_BOX.style.animation = "LeftBoxMove 1s ease-out";
+        RIGHT_BOX.style.animation = "RightBoxMove 1s ease-out";
     }
 });
