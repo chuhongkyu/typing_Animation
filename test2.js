aacd4adcd33a0dbf0b12fd7cd5a54e518ec4ed9b
Mr.chu <chuhongkyu@gmail.com>
2022-04-11 13:35:23 +0900

    update

--- test2.js
+++ test2.js
@@ -7,19 +7,12 @@ console.log(`Menu 종류 : ${Menu.length}`);
 let num1 = Math.random();
 let num2 = Math.floor(num1);
 let num3 = Math.floor(num1 * Menu.length);
-let num4 = Math.floor(Math.random() * Menu.length);
 
 // console.log("0/1 랜덤 : " + num1);
 // console.log("Floor  0 : " + num2);
 // console.log("정수 랜덤 : " + num3);
 // console.log("진짜 랜덤 : " + num4);
 
-
-// for(let i = 0; i< Menu.length; i++){
-//     if(i === 2){
-//     console.log(Menu[i].toString());
-//   }
-// }  
 
 // for(let i =0; i< Menu.length; i++){
 //     if(num4 == i){
@@ -28,8 +21,22 @@ let num4 = Math.floor(Math.random() * Menu.length);
 //     }
 // }
 
-Menu.forEach((item, index)=>{
-    if(num4 == index){
-        console.log(`랜덤 숫자: ${num4}, Menu 숫자: ${index}, 추천 메뉴: ${item}`);
-    }
-});
+// Menu.forEach((item, index)=>{
+//     if(num4 == index){
+//         console.log(`랜덤 숫자: ${num4}, Menu 숫자: ${index}, 추천 메뉴: ${item}`);
+//     }
+// });
+
+
+function recommend(){
+
+    let num4 = Math.floor(Math.random() * Menu.length);
+
+    Menu.forEach((item, index)=>{
+        if(num4 == index){
+            // console.log(`랜덤 숫자: ${num4}, Menu 숫자: ${index}, 추천 메뉴: ${item}`);
+            document.querySelector(".recommend").innerHTML = `<h5>추천 메뉴: <p style="color: red;">${item}</p></h5>`;
+        }
+    });
+    
+}
