c965b01c7ec1072719b317a3d50012ceabe9f56b
Mr.chu <chuhongkyu@gmail.com>
2022-04-13 11:32:06 +0900

    update

--- js/randomimg.js
+++ js/randomimg.js
@@ -1,19 +1,23 @@
 const Img_Arr = ["bg_1","bg_2","bg_3","bg_4","bg_5"];
 const Img_Main = document.getElementById("img_main");
+const Text_Arr = ["GET","LOVE","FREE","SUNDAY"];
+const Text_Main = document.getElementById("text_main");
 
 let path = "img/";
 let mark = ".jpg";
 
 window.onload = ()=>{
-    change();
+    onChangeImg();
 }
 ////요것을 해야 기존에 디폴트가 사라진다. 왜냐면 온로드가 되니까
 
-function change(){
+function onChangeImg(){
     let random = Math.floor(Math.random() * Img_Arr.length);
     Img_Main.src = path + Img_Arr[random] + mark;
+    let randomText = Math.floor(Math.random() * Text_Arr.length);
+    Text_Main.innerHTML = Text_Arr[randomText];
 }
-///// console.log("오류 체크");
+///// 함수로 빼야 또 쓰징~
 
-setInterval(change, 3000);
+setInterval(onChangeImg, 3000);
 
