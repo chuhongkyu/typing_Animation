ee4a7b33d6f2c602ad0c8af0f67e552ec4afcd57
Mr.chu <chuhongkyu@gmail.com>
2022-04-22 17:30:52 +0900

    pratice_obj

--- test.js
+++ test.js
@@ -1,75 +1,30 @@
-//객체란 무엇일까?
+// 객체 연습
 
-// const menu = ["민트초코", "바닐라라떼", "아메리카노", "녹차라떼"];
-// const price =[5000, 4500, 2000];
+const menu = [{
+  name: "민트초코",
+  price: 5000
+},
+{
+  name: "바닐라라떼",
+  price: 4500
+},
+{
+  name: "아메리카노",
+  price: 2000
+}]
 
+const orderText = document.querySelector(".order-text");
+const answerText = document.querySelector(".answer-text");
+const inputPanel = document.querySelector("#order-panel");
 
-// function orderPrint(i){
-//   console.log(`손님 ~ ${menu[i]}는 ${price[i]}원 입니다.   `);
-// }
+function howMuchIsit(){
+  let orderName = parseInt(inputPanel.value) -1;
 
-// orderPrint(1);
-// orderPrint(2);
-
-// function sumPrice(first, second){
-//   return price[first] + price[second];
-// }
-
-// let superPrice = sumPrice(1,2);
-
-// console.log(`총 ${superPrice}`);
-
-// const cafeMenu = {
-//   menu: "민트초코",
-//   price: 5000,
-// }
-
-// console.log(cafeMenu.menu);
-// console.log(cafeMenu["menu"]);
-
-// console.log(cafeMenu.price);
-// console.log(cafeMenu["price"]);
-
-
-//key 와 value 이름과 값
-
-const dog ={
-  name: "설",
-  age: 9,
-  kind: "요크셔테리어",
-  color:"실버",
-  skill: "구르기",
-  action: function(){
-    console.log("빵야");
-  },
-  action2: (say)=>{
-    console.log(say);
+  if(orderName < menu.length && orderName > -1){
+    orderText.innerHTML = `<h1>${menu[orderName].name }는 얼마인가요?</h1>`;
+    setTimeout(()=>{answerText.innerHTML = `<h4>${menu[orderName].name}는 ${menu[orderName].price}원 입니다.</h4>`;}, 1000);
+  }
+  else {
+    alert("메뉴를 입력해주세요. 메뉴는 3번까지 있습니다.");
   }
 }
-
-dog.action();
-dog.action2("죽은척");
-
-const me = {
-  name: "추홍규",
-  age: 29,
-  study: ["js", "css/html"],
-  skill: "딴짓",
-  team: "앱개발",
-  action: function(i){
-    if(i == 0){
-      console.log("밥먹으러가자");
-    }
-    else if(i == 1){
-      console.log("옥상가자");
-    }
-    else{
-      console.log("집가자");
-    }
-  }
-}
-
-console.log(me.study[0]);
-me.action();
-me.action(0);
-me.action(1);
