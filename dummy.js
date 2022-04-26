26eb109b5f7161842414f60f361532adef3df7f4
Mr.chu <chuhongkyu@gmail.com>
2022-04-26 13:48:00 +0900

    update

--- dummy.js
+++ dummy.js
@@ -1,13 +1,12 @@
-let booking;
-let booking2 = 0;
-let booking3 = -1;
-let booking4 = null;
-let booking5 = "";
-let booking6 = undefined;
+for(let i = 1; i <= 50; i++){
+  let stringValue = i.toString()
+  let result = "";
 
-console.log("1번째 : " + booking);
-console.log("2번째 : " + booking2);
-console.log("3번째 : " + booking3);
-console.log("4번째 : " + booking4);
-console.log("5번째 : " + booking5);
-console.log("6번째 : " + booking6);
+
+  for(let j = 0; j<stringValue.length; j++){
+    if(stringValue[j] == "3" ||stringValue[j] == "6" ||stringValue[j] == "9" ){
+      result+="짝"
+    }
+  }
+  console.log(result.length > 0 ? result : i);
+}
