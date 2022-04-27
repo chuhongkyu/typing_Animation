56258bbaf176af74bf56a20f558d6c61beca3e8b
Mr.chu <chuhongkyu@gmail.com>
2022-04-27 15:58:07 +0900

    Update dummy.js

--- dummy.js
+++ dummy.js
@@ -1,12 +1,23 @@
-for(let i = 1; i <= 50; i++){
-  let stringValue = i.toString()
-  let result = "";
+// for (let i = 1; i <= 50; i++) {
+//   let num = i.toString();
+//   let result = "";
+//   for(let j = 0; j < num.length; j++){
+//     if(num[j] == "3" || num[j] === "6" || num[j] === "9"){
+//       result+= "짝"
+//     }
+//   }
+//   console.log(result.length > 0 ? result: i);
+// }
 
+let n = 15
+let isPrime = true
 
-  for(let j = 0; j<stringValue.length; j++){
-    if(stringValue[j] == "3" ||stringValue[j] == "6" ||stringValue[j] == "9" ){
-      result+="짝"
-    }
-  }
-  console.log(result.length > 0 ? result : i);
+if (n === 1){// 1은 소수가 아님 
+    isPrime = false
 }
+for(let i=2;i<n;i++){
+  if(n % i == 0){
+    isPrime =  false 
+  }
+}
+console.log(isPrime)
